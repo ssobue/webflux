990104edb3b5a6823c6180506c614c9a3e9b57c0
SOBUE Sho <sho@sobue.dev>
2020-11-15 14:06:24 +0900

    Spring Boot 2.4.0, Gradle 6.7

--- src/main/java/dev/sobue/web/webflux/WebfluxConfiguration.java
+++ src/main/java/dev/sobue/web/webflux/WebfluxConfiguration.java
@@ -1,12 +1,6 @@
 package dev.sobue.web.webflux;
 
-import io.r2dbc.spi.ConnectionFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator;
-import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
-import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
 
 /**
  * Application Configuration.
@@ -16,27 +10,4 @@ import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePop
 @Configuration
 public class WebfluxConfiguration {
 
-  /**
-   * ConnectionFactoryInitializer for data initialization.
-   *
-   * @param connectionFactory r2dbc's connection factory
-   * @return Connection Factory Initializer
-   */
-  @Bean
-  public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
-    ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
-    initializer.setConnectionFactory(connectionFactory);
-
-    CompositeDatabasePopulator compositeDatabasePopulator = new CompositeDatabasePopulator();
-    compositeDatabasePopulator.addPopulators(
-        new ResourceDatabasePopulator(new ClassPathResource("/schema.sql"))
-    );
-    compositeDatabasePopulator.addPopulators(
-        new ResourceDatabasePopulator(new ClassPathResource("/data.sql"))
-    );
-
-    initializer.setDatabasePopulator(compositeDatabasePopulator);
-
-    return initializer;
-  }
 }
